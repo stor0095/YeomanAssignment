660745bfd9ded2823c77db04c529fe248635c1ad
stor0095 <geemakunstorey@Geemakuns-MacBook-Pro.local>
2016-10-14 15:52:40 -0400

    Initial Commit
    Initial

---
+++ app/scripts/controllers/geemakun.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yeomanApp.controller:GeemakunCtrl
+ * @description
+ * # GeemakunCtrl
+ * Controller of the yeomanApp
+ */
+angular.module('yeomanApp')
+  .controller('GeemakunCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
